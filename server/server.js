8d542ca9034505809ced87262d0be05773d80bc9
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-19 16:04:15 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log("new user connected");
     
-    socket.emit('newMessage', {
-        from: 'Server',
-        text: "Hello client from server",
-        createdAt: "1231331"
-    });
-
     socket.on('createMessage', (message)=>{
-        console.log('createMessage', message)
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on('disconnect',()=>{
