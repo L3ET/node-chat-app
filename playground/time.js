70117aac4e8c732ca7599e4f816a1b8977527bc0
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-20 18:15:34 +0530

    time to message

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+// var date = new Date();
+
+// console.log(date.getMonth());
+
+var date = moment();
+console.log(date.format('Do MMM,  YYYY'));
+
+console.log(date.format('h:mm a'));
