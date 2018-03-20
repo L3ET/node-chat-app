bee38814acadfcdc9f7b3daaddc5d59c2b06fea3
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-20 14:24:22 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ socket.on('newMessage', function (message){
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message){
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}:`);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e){
     e.preventDefault();
 
@@ -34,7 +44,10 @@ locationbutton.on('click', function (){
         return alert('Geolocation not supported by your browser.')
     }
     navigator.geolocation.getCurrentPosition(function(position){
-        console.log("sdf");
+        socket.emit('createLocationMessage',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
         console.log(position);
     }, function(){
         alert ('Unable to fetch location.');
