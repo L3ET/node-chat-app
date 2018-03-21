36d85255ddc3943c2f6362a6e419f3f5851cfda1
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-21 15:26:14 +0530

    Add Users class and update user list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,11 +14,29 @@ function scrollToBottom(){
     }
 }
 socket.on('connect', function() {
-    console.log("Connected to server");
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('No Error');
+        }
+    });
 });
 socket.on('disconnect', function() {
     console.log("Disconnacted from server")
  });
+
+socket.on('updateUserList', function (users){
+    var ol = jQuery('<ol></ol>');
+    users.forEach(function (user){
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+});
 
 socket.on('newMessage', function (message){
     var template = jQuery('#message-template').html();
@@ -33,6 +51,7 @@ socket.on('newMessage', function (message){
 });
 
 socket.on('newLocationMessage', function(message){
+    var params = jQuery.deparam(window.location.search);    
     var template = jQuery('#location-template').html();
     var formatedTime = moment(message.creatredAt).format('h:mm a');
     var html = Mustache.render(template,{
@@ -49,9 +68,9 @@ jQuery('#message-form').on('submit', function (e){
     e.preventDefault();
 
     var messageTextbox = jQuery('[name=message]');
-
+    var params = jQuery.deparam(window.location.search);
     socket.emit('createMessage', {
-        from: 'User',
+        from: params.name,
         text: messageTextbox.val()
     }, function(){
         messageTextbox.val('')
